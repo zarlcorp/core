3530546eec6ebd511bc01864211fa1aa90bfa1de
steven@zarl.dev <steven@zarl.dev>
2026-02-16 18:12:19 +0000

    use backtick raw string for logo constant

--- pkg/zstyle/logo.go
+++ pkg/zstyle/logo.go
@@ -3,10 +3,9 @@ package zstyle
 import "github.com/charmbracelet/lipgloss"
 
 // Logo is the zarlcorp ASCII art wordmark for TUI splash screens.
-const Logo = "" +
-	" _  _  _| _ _  _ _ \n" +
-	"/_(_|| |(_(_)| |_)\n" +
-	"               |   "
+const Logo = `_  _  _| _ _  _ _ 
+/_(_|| |(_(_)| |_)
+               |  `
 
 // StyledLogo returns the logo rendered with the given lipgloss style.
 func StyledLogo(s lipgloss.Style) string {
